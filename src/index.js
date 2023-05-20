5535ecab7a2f2f08bef59dafd473b4c00ab2841b
IvashkaVIP <ivashka.co@gmail.com>
2023-05-20 11:43:56 +0300

    begin coding

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
 
+//name.official - полное имя страны
+//capital - столица
+//population - население
+//flags.svg - ссылка на изображение флага
+//languages - массив языков
+
+//https://restcountries.com/v3.1/all?fields=name,flags`
+
 const DEBOUNCE_DELAY = 300;
+const BASE_URL = 'https://restcountries.com/v3.1/all';
+const ENDPOINT = '?fields=name,capital,population,flags,languages';
+
+const options = {};
+
+ fetch(`${BASE_URL}${ENDPOINT}`, options)
+   .then(resp => resp.json())
+   .then(data => console.log(data));
+
