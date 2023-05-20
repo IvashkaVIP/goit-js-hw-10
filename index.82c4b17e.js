0ca2ab5ec2c0a46f0a131ffa3657c208c5cb4f0b
IvashkaVIP <86344094+IvashkaVIP@users.noreply.github.com>
2023-05-20 08:44:24 +0000

    Deploying to gh-pages from @ IvashkaVIP/goit-js-hw-10@5535ecab7a2f2f08bef59dafd473b4c00ab2841b 🚀

---
+++ index.82c4b17e.js
@@ -0,0 +1,2 @@
+fetch("".concat("https://restcountries.com/v3.1/all").concat("?fields=name,capital,population,flags,languages"),{}).then((function(n){return n.json()})).then((function(n){return console.log(n)}));
+//# sourceMappingURL=index.82c4b17e.js.map
---
+++ index.9644f816.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages",{}).then((e=>e.json())).then((e=>console.log(e)));
+//# sourceMappingURL=index.9644f816.js.map
