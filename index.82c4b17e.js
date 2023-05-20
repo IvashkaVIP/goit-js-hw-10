4125bbc040b8d1d0475edda061c386e8c855a92a
IvashkaVIP <86344094+IvashkaVIP@users.noreply.github.com>
2023-05-20 10:26:24 +0000

    Deploying to gh-pages from @ IvashkaVIP/goit-js-hw-10@83e607f9b191ba1fe7193b87b297bbdcac6106b1 🚀

--- index.82c4b17e.js
+++
@@ -1,2 +0,0 @@
-fetch("".concat("https://restcountries.com/v3.1/all").concat("?fields=name,capital,population,flags,languages"),{}).then((function(n){return n.json()})).then((function(n){return console.log(n)}));
-//# sourceMappingURL=index.82c4b17e.js.map
--- index.9644f816.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages",{}).then((e=>e.json())).then((e=>console.log(e)));
-//# sourceMappingURL=index.9644f816.js.map
